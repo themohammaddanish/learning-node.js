f2b6692fb5a5cef271e2a2b55c1e4b2bf281625f
Md Danish Khan <mddanish@nbc.edu.np>
2025-04-24 17:32:40 +0545

    .

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ function add(a,b) {
 
 console.log(add(4,5));
 const fs = require("fs")
-fs.writeFileSync("danish.txt", "hey its me danish khan im a dev")
+// fs.writeFileSync("danish.txt", "hey its me danish khan im a dev")
+fs.writeFile("danish2.txt", "danish is a good boy", ()=>{
+  console.log("done")
+  fs.readFile("danish2.txt", (error, data)=>{
+    console.log(error, data.toString())
+  })
+})
