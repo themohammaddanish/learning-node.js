e6e37c90c06a59e0704569b76d44734f95a8630d
Md Danish Khan <mddanish@nbc.edu.np>
2025-04-24 20:26:59 +0545

    .

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 console.log("hello world");
-function add(a,b) {
-  return a+b;
-  
+function add(a, b) {
+  return a + b;
 }
 
-console.log(add(4,5));
-const fs = require("fs")
+console.log(add(4, 5));
+const fs = require("fs");
 // fs.writeFileSync("danish.txt", "hey its me danish khan im a dev")
-fs.writeFile("danish2.txt", "danish is a good boy", ()=>{
-  console.log("done")
-  fs.readFile("danish2.txt", (error, data)=>{
-    console.log(error, data.toString())
-  })
-})
+fs.writeFile("danish2.txt", "danish is a good boy", () => {
+  console.log("done");
+  fs.readFile("danish2.txt", (error, data) => {
+    console.log(error, data.toString());
+  });
+});
 
-fs.appendFile("danish.txt" , "this text is added" , ()=> {
-  fs.readFile("danish.txt", (error,data)=>{
-    console.log(error, data.toString())
-  })
-})
+fs.appendFile("danish.txt", "this text is added", () => {
+  fs.readFile("danish.txt", (error, data) => {
+    console.log(error, data.toString());
+  });
+});
