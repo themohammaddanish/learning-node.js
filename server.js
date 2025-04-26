f5fbd03f7d8b27bac3c71a5bb75ef4ecb821c61a
Md Danish Khan <mddanish@nbc.edu.np>
2025-04-26 20:06:29 +0545

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,9 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello im  ");
+  res.send("🚀 Server is  NEW changes!");
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
