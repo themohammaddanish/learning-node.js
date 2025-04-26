e78b798088ceb14e5e3fb645be1efce8c485722c
Md Danish Khan <mddanish@nbc.edu.np>
2025-04-26 19:30:47 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+const express = required("express")
+const app = express()
+const port = 3000 
 console.log("hello world");
 function add(a, b) {
   return a + b;
 }
 
 console.log(add(4, 5));
+
 const fs = require("fs");
 // fs.writeFileSync("danish.txt", "hey its me danish khan im a dev")
+
 fs.writeFile("danish2.txt", "danish is a good boy", () => {
   console.log("done");
   fs.readFile("danish2.txt", (error, data) => {
