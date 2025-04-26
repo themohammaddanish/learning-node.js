1110f3f38520b79ad161d3ad4c6a50880448523d
Md Danish Khan <mddanish@nbc.edu.np>
2025-04-26 19:55:46 +0545

    .

--- server.js
+++ server.js
@@ -14,4 +14,15 @@
 //   });
 // });
 
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello im  ");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
